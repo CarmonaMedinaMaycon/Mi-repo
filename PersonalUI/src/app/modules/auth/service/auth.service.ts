808f4547332751deee4f093e86af468dfe1d51cb
CarmonaMedinaMaycon <105668860+CarmonaMedinaMaycon@users.noreply.github.com>
2022-11-17 09:57:49 -0600

    Add files via upload

---
+++ PersonalUI/src/app/modules/auth/service/auth.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { UserLogin } from '../types/user';
+import { APP_URL } from '../../../services/base-url';
+import { Router, Routes } from '@angular/router';
+import { catchError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private isLoading: boolean = false;
+  get loading(){
+    return this.isLoading;
+  }
+  
+  public set setLoading(value : boolean) {
+    this.isLoading= value;
+  }
+  
+  constructor( private readonly http:HttpClient, private router: Router) { }
+
+
+  signin(user:UserLogin){
+    this.setLoading=true;
+    this.http.post<any>(`${APP_URL}api/auth/`, user, {
+      headers:{'Content-Type':'aplication/json'},
+    })
+    .pipe(
+      catchError((error)=>{
+        this.setLoading=false;
+        return error;
+      })
+    )
+    .subscribe((Response)=>{
+      localStorage.setItem("token", Response.token);
+      this.router.navigateByUrl('');
+      this.setLoading=false;
+    });
+  }
+}
