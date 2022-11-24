3329c7a15de467bedb2564aa5a3d68c8cef04f19
CarmonaMedinaMaycon <105668860+CarmonaMedinaMaycon@users.noreply.github.com>
2022-11-24 09:37:47 -0600

    esta MAL
    esta mal

--- PersonalUI/src/app/modules/personal/pages/main-personal/main-personal.component.ts
+++ PersonalUI/src/app/modules/personal/pages/main-personal/main-personal.component.ts
@@ -1,14 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MathTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { PersonalService } from '../../service/personal.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { Personal } from '../../types/personal';
 
 @Component({
   selector: 'app-main-personal',
   templateUrl: './main-personal.component.html'
 })
 export class MainPersonalComponent implements OnInit {
-
-  constructor() { }
+displayedColums: String[]=[
+  "#",
+  "name",
+  "surname",
+  "lastname",
+  "birthday",
+  "salary",
+  "action"
+]; 
+@ViewChild(MatPaginator) paginator!: MatPaginator;
+@ViewChild(MatSort) sort!: MatSort;
+personal!:MathTableDataSource<Personal>;
+  constructor(private personalService:PersonalService,
+    private _liveAnnouncer:LiveAnnouncer,
+    public dialog:MatDialog) { }
 
   ngOnInit(): void {
+    this.personalService.findAll().subscribe((Personal[])=>{
+      this.personal = new MathDataSource<Personal>(response);
+      this.personalService.loading=false;
+      this.personal.sort=this.sort;
+    });
   }
+  announceSortChange(sort:Sort){
+    if(sort.direction){
+    this._liveAnnouncer.announce(`Sorted ${sort.direction} ending`);
+    }else{
+      this._liveAnnouncer.announce(`Sort cleared`);
+    }
+
+  }
+  //Modal
 
 }
